8ef54689fa5bccab15a5cd4308fbc73d03444c17
Sl0mek <andrzejslomkapraca@gmail.com>
2022-08-01 22:00:26 +0200

    Adding bitset<n> example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include "TransformData.hpp"
 #include <iostream>
+#include <bitset>
 
 int main(int argc, char const *argv[]) {
   std::cout << "Podaj liczbe: " << std::endl;
   int val = 0;
   std::cin >> val;
   std::cout << std::endl << TransformData::DecToBin(val) << std::endl;
+  std::bitset<8> bitSet(val);
+  std::cout << std::endl << bitSet << std::endl;
   return 0;
 }
